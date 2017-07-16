5f64295beaba51c87f45716f8dd8398edc5f5d1a
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-16 15:40:14 -0600

    adds categories to all tests

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -9,6 +9,9 @@ describe "User creates a new job" do
     fill_in "job[description]", with: "So fun!"
     fill_in "job[level_of_interest]", with: 80
     fill_in "job[city]", with: "Denver"
+    select("Web Development", from: "categorySelect").select_option
+
+
 
     click_button "Create"
 
