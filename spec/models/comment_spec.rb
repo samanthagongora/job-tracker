874c8b5054ab1575b65448ffe8d1c5583212aa55
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-17 11:39:49 -0600

    adds comment model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without content" do
+        comment = Comment.new()
+
+        expect(comment).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with content and job" do
+        job = create(:job)
+        comment = create(:comment, job: job)
+
+        expect(comment).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "belongs to a job" do
+      comment = create(:comment)
+
+      expect(comment).to respond_to(:job)
+    end
+  end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Contact, type: :model do
   describe "validations" do
     context "invalid attributes" do
-      it "is invalid without content" do
+      it "is invalid without info" do
         contact = Contact.new()
 
         expect(contact).to be_invalid
@@ -11,7 +11,7 @@ RSpec.describe Contact, type: :model do
     end
 
     context "valid attributes" do
-      it "is valid with a title, level of interest, and company" do
+      it "is valid with attributes and company" do
         company = create(:company)
         contact = create(:contact, company: company)
 
@@ -23,7 +23,7 @@ RSpec.describe Contact, type: :model do
   describe "relationships" do
     it "belongs to a company" do
       contact = create(:contact)
-      
+
       expect(contact).to respond_to(:company)
     end
   end
