9134d8399680db2b0c0ff2db8e56e49726f27fcf
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-17 11:43:43 -0600

    adds category model tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without title" do
+        category = Category.new()
+
+        expect(category).to be_invalid
+      end
+
+      it "is invalid without unique title" do
+        category1 = create(:category)
+        category2 = Category.new(title: category1.title)
+
+        expect(category2).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with title" do
+        category = create(:category)
+
+        expect(category).to be_valid
+      end
+    end
+  end
 end
